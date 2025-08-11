eceb55d9d3b2a6bfeaa7de850dfdc7eae5ae4e19
Mehdi Rizvi <muntazer.mehdi.rizvi@gmail.com>
2025-08-12 01:54:58 +0500

    Feat: Updated routing to accommodate new profile creation flow.

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,25 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+
+import {
+  SplashRoute,
+  WalkthroughRoute,
+  OnboardingRoute,
+  SignUpRoute,
+  LoginRoute,
+  ProfileCreationRouteModel,
+  FeedRoute,
+  CampaignsRoute,
+  CampaignDetailRoute,
+  CampaignCreateRoute,
+  MatchRoute,
+  ChatRoute,
+  SearchRoute,
+  ProfileRoute,
+  UserDetailRoute,
+} from "./pages/Index";
 
 const queryClient = new QueryClient();
 
@@ -15,8 +32,31 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+
+          {/* Common */}
+          <Route path="/" element={<SplashRoute />} />
+          <Route path="/walkthrough" element={<WalkthroughRoute />} />
+          <Route path="/onboarding" element={<OnboardingRoute />} />
+          <Route path="/signup" element={<SignUpRoute />} />
+          <Route path="/login" element={<LoginRoute />} />
+
+          {/* Model */}
+          <Route path="/model/profile-creation" element={<ProfileCreationRouteModel />} />
+
+          {/* Main Routes */}
+          <Route path="/feed" element={<FeedRoute />} />
+          <Route path="/campaigns" element={<CampaignsRoute />} />
+          <Route path="/campaign/:id" element={<CampaignDetailRoute />} />
+          <Route path="/campaign-create" element={<CampaignCreateRoute />} />
+          <Route path="/match" element={<MatchRoute />} />
+          <Route path="/chat" element={<ChatRoute />} />
+          <Route path="/search" element={<SearchRoute />} />
+          <Route path="/profile" element={<ProfileRoute />} />
+          <Route path="/user/:id" element={<UserDetailRoute />} />
+
+          {/* Agency */}
+
+          {/* Fallback */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
