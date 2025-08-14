93fda2c67e90dbe027ee8023ea82997b15d1d91d
Mehdi Rizvi <muntazer.mehdi.rizvi@gmail.com>
2025-08-14 12:36:04 +0500

    feat(store): create Zustand store for campaigns
    - Added global store to manage campaigns state.
    - Includes fetchCampaigns and getCampaignById (local lookup) methods.
    - Centralized campaigns data to avoid multiple API calls.

---
+++ src/stores/campaignStore.ts
@@ -0,0 +1,33 @@
+// src/stores/campaignStore.ts
+import  { create } from 'zustand';
+import { getCampaigns, Campaign } from '../services/getCampaigns';
+
+interface CampaignState {
+  campaigns: Campaign[];
+  loading: boolean;
+  error: string | null;
+  fetchCampaigns: () => Promise<void>;
+  getCampaignById: (id: number) => Campaign | undefined;
+}
+
+export const useCampaignStore = create<CampaignState>((set, get) => ({
+  campaigns: [],
+  loading: false,
+  error: null,
+
+  fetchCampaigns: async () => {
+    set({ loading: true, error: null });
+    try {
+      const data = await getCampaigns();
+      set({ campaigns: data });
+    } catch (err: any) {
+      set({ error: err.message });
+    } finally {
+      set({ loading: false });
+    }
+  },
+
+  getCampaignById: (id: number) => {
+    return get().campaigns.find((c) => c.id === id);
+  },
+}));
